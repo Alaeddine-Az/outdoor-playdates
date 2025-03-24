9c5b4ee41b59c9231df5f2bc9c7edfdf16673658
Alaeddine-Az <68476555+Alaeddine-Az@users.noreply.github.com>
2025-03-23 23:27:29 -0600

    Update DesktopNav.tsx
    add Get started button in nav menu

--- src/components/header/DesktopNav.tsx
+++ src/components/header/DesktopNav.tsx
@@ -59,6 +59,11 @@ const DesktopNav = ({ user, scrollToSection, handleSignOut }: DesktopNavProps) =
               Sign In
             </Button>
           </Link>
+          <Link to="/auth">
+            <Button variant="outline" className="border-primary text-primary hover:bg-primary hover:text-white">
+              Get Started
+            </Button>
+          </Link>
         </>
       )}
     </nav>
